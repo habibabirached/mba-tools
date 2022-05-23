f4618e3a15d2421d969132cfd09e079abd048c09
James Jobin <200021446@ge.com>
2022-05-23 13:32:53 -0700

    Added comments.

--- Windows-and-Office-Updates.ps1
+++ Windows-and-Office-Updates.ps1
@@ -1,12 +1,17 @@
 
 # One time install modules. Don't need to do it after that.
+# Can be commented out.
 
 Install-Module PSWindowsUpdate
 Add-WUServiceManager -MicrosoftUpdate
 
+# This is to install the updates. Will be needed each time.
+
 Install -WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
 
 Timeout /T 10
+
+# For the Office updates
 
 cd 'C:\Program Files\Common Files\microsoft shared\ClickToRun\'
 ./Office2RClient.exe /update user
